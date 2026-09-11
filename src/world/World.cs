bdceacfb9de53328befb8eb16bc3e7583ceba496
Tschakala <raffael.brauner@outlook.com>
2026-09-11 12:23:33 +0200

    World Generation

--- src/world/World.cs
+++ src/world/World.cs
@@ -15,6 +15,9 @@ public partial class World : Node2D
 	private FastNoiseLite _noise =  new FastNoiseLite();
 	[Export] private float _frequence  = 0.02f;
 	
+	//random Number
+	private RandomNumberGenerator _rng = new RandomNumberGenerator();
+	
 	public override void _Ready()
 	{
 		GenerateBackground();		
@@ -23,11 +26,11 @@ public partial class World : Node2D
 
 	private void GenerateBackground()
 	{
-		for (int i = 0; i < _mapSize; i++)
+		for (int i = -(_mapSize / 2); i < _mapSize; i++)
 		{
-			for (int j = 0; j < _mapSize; j++)
+			for (int j = -(_mapSize / 2); j < _mapSize; j++)
 			{
-				DrawColumn(_backGround,new Vector2I(i, j), 0, new Vector2I(8, 2));
+				DrawColumn(_backGround,new Vector2I(i, j), 0, new Vector2I(5, 1));
 			}
 		}
 	}
@@ -38,23 +41,77 @@ public partial class World : Node2D
 		_noise.Frequency = _frequence;
 		
 
-		for (int i = 0; i < _mapSize; i++)
+		for (int i = -(_mapSize / 2); i < _mapSize; i++)
 		{
-			for (int j = 0; j < _mapSize; j++)
+			for (int j = -(_mapSize / 2); j < _mapSize; j++)
 			{
 				float noiseValue = _noise.GetNoise2D(i, j);
-				// noiseValue += 1;
-				// noiseValue /= 2;
 				noiseValue = (noiseValue + 1) / 2; //Normalize from [-1;1] to [0;1]
 				//GD.Print(noiseValue);
-
-				if (noiseValue >= 0.5)
+				if ((i == -(_mapSize / 2) || i == _mapSize - 1 || j ==  -(_mapSize / 2) || j == _mapSize - 1) || (i == -(_mapSize / 2) + 1 || i == _mapSize - 2 || j ==  -(_mapSize / 2) + 1 || j == _mapSize - 2)) // walls around the map
 				{
-					DrawColumn(_walls,new Vector2I(i,j),3, new Vector2I(3, 1));
+					if (i == -(_mapSize / 2) + 1)
+					{
+						DrawColumn(_walls,new Vector2I(i,j),3, new Vector2I(6, _rng.RandiRange(2, 5)));
+					}
+					else if (i == _mapSize - 2)
+					{
+						DrawColumn(_walls,new Vector2I(i,j),3, new Vector2I(1, _rng.RandiRange(2, 5)));
+					}
+					else if (j ==  -(_mapSize / 2) + 1)
+					{
+						DrawColumn(_walls,new Vector2I(i,j),3, new Vector2I(_rng.RandiRange(2, 5), 6));
+					}
+					else if (j == _mapSize - 2)
+					{
+						DrawColumn(_walls,new Vector2I(i,j),3, new Vector2I(_rng.RandiRange(2, 5), 1));	
+					}
+					else if (i == -(_mapSize / 2))
+					{
+						DrawColumn(_walls,new Vector2I(i,j),3, new Vector2I(8, _rng.RandiRange(2, 5)));
+					}
+					else if (i == _mapSize - 1)
+					{
+						DrawColumn(_walls,new Vector2I(i,j),3, new Vector2I(13, _rng.RandiRange(2, 5)));
+					}
+					else if (j ==  -(_mapSize / 2))
+					{
+						DrawColumn(_walls,new Vector2I(i,j),3, new Vector2I(_rng.RandiRange(9, 12), 1));
+					}
+					else
+					{
+						DrawColumn(_walls,new Vector2I(i,j),3, new Vector2I(_rng.RandiRange(9, 12), 6));
+					}
 				}
 				else
 				{
-					DrawColumn(_path,new Vector2I(i,j),1, new Vector2I(3, 3));
+					if (noiseValue >= 0.5)
+					{
+						if (noiseValue >= 0.5 && noiseValue < 0.52)
+						{
+							DrawColumn(_walls,new Vector2I(i,j),3, new Vector2I(3, 1));	
+						}
+						else if (noiseValue >= 0.52 && noiseValue < 0.54)
+						{
+							DrawColumn(_walls,new Vector2I(i,j),3, new Vector2I(3, 2));						
+						}
+						else
+						{ 
+							DrawColumn(_walls,new Vector2I(i,j),3, new Vector2I(28, 3));	
+						}
+					}
+					else
+					{
+						if (j % 2 == 0)
+						{
+							DrawColumn(_path,new Vector2I(i,j),1, new Vector2I(16, 5));	
+						}
+						else
+						{
+							DrawColumn(_path,new Vector2I(i,j),1, new Vector2I(16, 6));
+						}
+					
+					}
 				}
 			}
 		}
